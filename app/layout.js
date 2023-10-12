7b1b226de867ee6b5a9cb25dcd410aac1417a035
Matheus Castro <mate.cas37@gmail.com>
2023-10-12 11:55:04 -0300

    feat: tela de login Criada.

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,28 @@
+import './global.css';
+
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'swap',
+})
+
 export const metadata = {
   title: 'Lista de Tarefas',
   description: 'Anote suas tarefas!',
+  icons: {
+    icon: './icon.ico',
+  }
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <body>{children}</body>
+      <body className={roboto.className}>
+        {children}
+      </body>
     </html>
   )
 }
--- app/page.js
+++ app/page.js
@@ -1,6 +1,36 @@
+import styles from './page.module.css';
+
+import TextField from '@mui/material/TextField';
+
+import Image from "next/image";
+import img_login from '../public/img_login.jpg';
+import img_icon from '../app/icon.ico';
+import Link from 'next/link';
+
 export default function Home() {
   return (<>
-    <h1>Hello, World</h1>
-    <p>Listagem de tarefas</p>
+    <main className={styles.main}>
+      <Image className={styles.img_login} src={img_login} height={515} width={450} style={{objectFit: 'cover'}} />
+
+      <Image className={styles.iconLogin} src={img_icon} height={120} width={120} />
+
+      <div className={styles.login}>
+        
+        <h2 className={styles.title}>Login</h2>
+        <b>Logue e veja suas tarefas!</b>
+        <hr/>
+        <form className={styles.form}>
+          <label className={styles.label}>E-MAIL:</label>
+          <TextField id="outlined-basic" label="Digite seu E-mail..." variant="outlined" />
+          <label className={styles.label}>SENHA:</label>
+          <TextField id="outlined-basic" label="Digite sua Senha" variant="outlined" />
+        
+          <button className={styles.btn}>Login</button>
+          <p className={styles.createUser}>Não possue conta?<Link className={styles.link} href='/'>Criar Conta</Link></p>
+
+        </form>
+      
+      </div>
+    </main>
   </>)
 }
