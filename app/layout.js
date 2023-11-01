0c73e2920afa582d11de4a050fe55c9e8ebd3bec
Matheus Castro <mate.cas37@gmail.com>
2023-10-31 21:24:11 -0300

    feat: Removendo metadata do layout e adicionando react query.

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,9 @@
+'use client'
+
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 import './global.css';
 
 import { Roboto } from 'next/font/google';
@@ -9,19 +15,15 @@ const roboto = Roboto({
   display: 'swap',
 })
 
-export const metadata = {
-  title: 'Lista de Tarefas',
-  description: 'Anote suas tarefas!',
-  icons: {
-    icon: './icon.ico',
-  }
-}
+const queryClient = new QueryClient()
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <body className={roboto.className}>
-        {children}
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
       </body>
     </html>
   )
