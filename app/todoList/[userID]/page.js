7e5b51180431cb007ad025a8f14cd3642b53a8a2
Matheus Castro <mate.cas37@gmail.com>
2023-11-11 19:37:35 -0300

    feat: criando modal de tarefas.

--- app/todoList/[userID]/page.js
+++ app/todoList/[userID]/page.js
@@ -2,10 +2,13 @@ import styles from './page.module.css';
 import TodoList from './todoListComponent';
 
 import AddIcon from '@mui/icons-material/Add';
+import ClearIcon from '@mui/icons-material/Clear';
+import CheckIcon from '@mui/icons-material/Check';
 
 export default function TodoPage({ params }) {
 
     return(<>
+
         <main className={styles.container}>
             <section className={styles.firstLayer}>
                 <h1>ToDo List</h1>
@@ -14,10 +17,22 @@ export default function TodoPage({ params }) {
 
             <TodoList user={params.userID}/>
 
-            <section className={styles.addTodo}>
-                <button><AddIcon /></button>
-            </section>
-
         </main>
+
+        <section className={styles.addTodoModal}>
+            <ClearIcon />
+            <label>Nome da Tarefa:</label>
+            <input placeholder='Digite uma tarefa...' />
+
+            <div className={styles.btnGroup}>
+                <button>Adiconar <CheckIcon /></button>
+                <button>Ainda não <ClearIcon /></button>
+            </div>
+        </section>
+
+        <section className={styles.addTodo}>
+            <button><AddIcon /></button>
+        </section>
+    
     </>)
 }
