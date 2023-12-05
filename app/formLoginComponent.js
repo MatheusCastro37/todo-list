21eff64adb88a79abfa49dd7c6e8cfa7250d5d26
Matheus Castro <mate.cas37@gmail.com>
2023-12-05 10:31:14 -0300

    refactor: remoção include.

--- app/formLoginComponent.js
+++ app/formLoginComponent.js
@@ -35,7 +35,6 @@ export default function FormLogin() {
                     'Accept': 'application/json',
                     'Content-Type': 'application/json',
                 },
-                credentials: 'include',
                 body: JSON.stringify(loginUser)
             })
 
