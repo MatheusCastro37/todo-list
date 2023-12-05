6bc891fb13d83856cbfacf9d056e86e7c6526a2e
Matheus Castro <mate.cas37@gmail.com>
2023-12-05 10:38:21 -0300

    Revert "feat: validando existencia do cookie."
    This reverts commit a10e42995d0689b00aa31fd62a10d91ea7c69bdc.

--- app/todoList/page.js
+++ app/todoList/page.js
@@ -1,32 +1,22 @@
 import styles from './page.module.css';
 import TodoList from './todoListComponent';
 import Modal from './modalComponent';
-import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
 
 export default function TodoPage() {
 
-    const cookieAPI = cookies().has('tokenAPI')
-
     return(<>
 
-        {
-            cookieAPI ?
-            <>
-                <main className={styles.container}>
-                    <section className={styles.firstLayer}>
-                        <h1>ToDo List</h1>
-                        <a href='#' className={styles.quit}>Sair</a>
-                    </section>
+        <main className={styles.container}>
+            <section className={styles.firstLayer}>
+                <h1>ToDo List</h1>
+                <a href='#' className={styles.quit}>Sair</a>
+            </section>
 
-                    <TodoList />
+            <TodoList />
 
-                </main>
+        </main>
 
-                <Modal />
-            </>
-            : redirect('/')
-        }
+        <Modal />
     
     </>)
 }
